842700527718d55880042742396ea1a01e69f443
Sahus Nulu <53015701+Insainian@users.noreply.github.com>
2020-08-13 14:19:45 -0700

    Integrate OAuth libraries

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { GlossaryModule } from './glossary/glossary.module';
-import { SearchModule } from './search/search.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UserModule } from './user/user.module'
+import { GlossaryModule } from './glossary/glossary.module'
+import { SearchModule } from './search/search.module'
+import { GoogleModule } from './Google OAuth/google.module'
 
 @Module({
-  imports: [UserModule, GlossaryModule, SearchModule],
+  imports: [UserModule, GlossaryModule, SearchModule, GoogleModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
